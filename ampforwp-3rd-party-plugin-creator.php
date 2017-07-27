b7b8ab5ad7d5cb3f52d27918aca10260a79324b4
Mohammed Razzaq <marqas36@gmail.com>
2017-07-27 20:27:33 +0530

    Section, Options, and Functionality has been added
    Almost ready, need some more testing

--- ampforwp-3rd-party-plugin-creator.php
+++ ampforwp-3rd-party-plugin-creator.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Plugin Name: AMPforWP 3rd Party Plugin Creator 
+Plugin Name: AMPforWP Plugin Manager
 Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
 Description: MU Plugin Creator for Accelerated Mobile Pages
 Version: 1.0
@@ -37,4 +37,97 @@ function  ampforwp_plugin_supporter_deactivation_hook() {
 		@unlink( WPMU_PLUGIN_DIR . "/ampforwp-plugin-supporter.php" );
 	}
 
+}
+
+if ( defined( 'AMPFORWP_PLUGIN_DIR' ) ) { 
+	add_filter( 'plugin_action_links', 'ampforwp_plugin_creator_settings_link', 10, 5 );
+} else {
+
+	add_filter( 'plugin_action_links', 'ampforwp_plugin_creator_activation_link', 10, 5 );
+
+	// Add Activate Parent Plugin button in settings page
+	if ( ! function_exists( 'ampforwp_plugin_creator_activation_link' ) ) {
+		function ampforwp_plugin_creator_activation_link( $actions, $plugin_file ) {
+			static $plugin;
+			if (!isset($plugin))
+				$plugin = plugin_basename(__FILE__);
+				if ($plugin == $plugin_file) {
+						$settings = array('settings' => '<a href="plugin-install.php?s=accelerated+mobile+pages&tab=search&type=term">' . __('Please Activate the Parent Plugin.', 'ampforwp_plugin_creator') . '</a>');
+						$actions = array_merge($settings , $actions );
+					}
+				return $actions;
+		}
+	}
+	// Return if Parent plugin is not active, and don't load the below code.
+	return;
+}
+
+// Add settings Icon in the plugin activation page
+if ( ! function_exists( 'ampforwp_plugin_creator_settings_link' ) ) {
+	function ampforwp_plugin_creator_settings_link( $actions, $plugin_file )  {
+			static $plugin;
+			if (!isset($plugin))
+				$plugin = plugin_basename(__FILE__);
+				if ($plugin == $plugin_file) {
+						$settings = array('settings' => '<a href="admin.php?page=amp_options&tab=8">' . __('Settings', 'ampforwp_plugin_creator') . '</a>');
+			  		$actions = array_merge( $actions , $settings);
+					}
+				return $actions;
+	}
+}
+function get_the_active_plugins() {
+    $plugins = array();
+    $plugins = get_option ( 'active_plugins' );
+    $plugins = array_flip($plugins);
+    return $plugins;
+}
+function list_the_plugins_names(){
+    $new_array = array();
+    $arrray = get_the_active_plugins();
+    foreach ($arrray as $key => $value) {
+        $new_array[ $key] =  $key;
+    }
+    return $new_array;
+}
+add_filter("redux/options/redux_builder_amp/sections", 'ampforwp_settings_to_disable_plugins');
+
+if ( ! function_exists( 'ampforwp_settings_to_disable_plugins' ) ) {
+			function ampforwp_settings_to_disable_plugins($sections){
+
+		$sections[] = array(
+	        'title' => __('AMPforWP Plugin Manager', 'redux-framework-demo'),
+	        'icon' => 'el el-th-large',
+					'desc'  => 'Disable Plugins in AMP',
+	        'fields' =>  ampforwp_create_controls_for_plugin_manager(),
+	        );
+
+return $sections;
+}
+}
+
+function ampforwp_create_controls_for_plugin_manager(){
+	  $controls[]  = 	array(
+                        'id'        =>'amp-plugin-manager-switch',
+                        'type'      => 'switch',
+                        'title'     => __('Plugin Manager', 'accelerated-mobile-pages'),
+                        'default'   => 0, 
+                        'true'      => 'true',
+                        'false'     => 'false',
+                    );
+
+       $controls[]  =    array(
+                        'id'       => 'amp-pm',
+                        'type'     => 'checkbox',
+                        'title'    => __('Select plugins to disable', 'accelerated-mobile-pages'),
+                        'required' => array('amp-plugin-manager-switch', '=', 1),
+                        'options'     => list_the_plugins_names(),
+                    );
+
+         return $controls;  
+ }
+add_action('redux/options/redux_builder_amp/saved', 'update_plugin_data');
+function update_plugin_data(){
+	global $redux_builder_amp; 
+	$get_data_from_redux = $redux_builder_amp['amp-pm']; 
+	 update_option('new_mu_plugins', $get_data_from_redux);
 }
--- plugin/ampforwp-plugin-supporter.php
+++ plugin/ampforwp-plugin-supporter.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: AMPforWP 3rd Party Plugin Supporter 
+Plugin Name: AMPforWP Plugin Manager Supporter 
 Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
 Description: Makes the AMP pages valid by disabling error causing plugins on AMP pages.
 Version: 1.0
@@ -15,11 +15,31 @@ $current_url   = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '';
 if ( strstr( $current_url, $listener_term ) ) {
 	add_filter( 'option_active_plugins', 'ampforwp_api_request_disable_plugin' , 100);
 }
+
+
+function get_active_plugins_list(){
+
+	$list_of_plugins = array();
+	$list_of_selected_plugins = '';
+
+	$list_of_selected_plugins = get_option('new_mu_plugins');
+		
+	$list_of_selected_plugins = array_filter($list_of_selected_plugins);
+		
+	
+	foreach ($list_of_selected_plugins as $key => $value) {
+		$list_of_plugins[] =  $key;
+	}
+	return $list_of_plugins;
+}
+
+
 function ampforwp_api_request_disable_plugin( $plugins ) {
 
-	$plugins_not_needed = array(
-		'squirrly-seo/squirrly.php'
-	);
+
+
+
+	$plugins_not_needed = get_active_plugins_list();
 
 	foreach ( $plugins_not_needed as $plugin ) {
 		$key = array_search( $plugin, $plugins );
@@ -30,3 +50,4 @@ function ampforwp_api_request_disable_plugin( $plugins ) {
 
 	return $plugins;
 }
+
